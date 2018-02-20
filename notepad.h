b81b6adcf062d12cdd4b9e156cea9ce3f5948c18
Miroslav Semerad <miretz@gmail.com>
2018-02-20 22:34:05 +0100

    Initial commit

---
+++ notepad.h
@@ -0,0 +1,38 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class Notepad;
+}
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Notepad(QWidget *parent = 0);
+    ~Notepad();
+
+private slots:
+    void on_actionNew_triggered();
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionExit_triggered();
+    void on_actionCopy_triggered();
+    void on_actionCut_triggered();
+    void on_actionPaste_triggered();
+    void on_actionUndo_triggered();
+    void on_actionRedo_triggered();
+    void on_actionFont_triggered();
+    void on_actionSave_As_triggered();
+    void on_actionSearch_triggered();
+    void on_searchButton_clicked();
+
+private:
+    Ui::Notepad *ui;
+    QString currentFile;
+};
+
+#endif // NOTEPAD_H
